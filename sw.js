34f4fe23c26fc9421d08564cef3f226a1ab72c8d
sergeolitegui-ai <sergeolitegui@gmail.com>
2026-03-09 00:33:17 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'quemagrama-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Syne:wght@400;600;800&family=JetBrains+Mono:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
